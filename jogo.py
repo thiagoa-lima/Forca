be0e79663efcd028f5976fbf6cdfdad9550fcf77
thiagoandradelima <thiagog_12@hotmail.com>
2023-01-03 14:20:55 -0300

    Mensagem de início do jogo
    Inclusao da mensagem de abertura do jogo com opção de escolher qual nível se quer jogar

--- jogo.py
+++ jogo.py
@@ -7,16 +7,31 @@ print("******************************************************")
 print(" ")
 
 time.sleep(1)
+nivel = 0
 
-print("Você tem a opção de escolher os níveis conforme abaixo")
-print(" ")
-print("[ 1 ] Nível fácil - você terá 10 chances")
-print("[ 2 ] Nível médio - você terá 7 chances")
-print("[ 3 ] Nível difícil - você terá 4 chances")
-print(" ")
+while nivel != 1 and nivel != 2 and nivel != 3:
+    print("Você tem a opção de escolher os níveis conforme abaixo")
+    print(" ")
+    print("[ 1 ] Nível fácil - você terá 10 chances")
+    print("[ 2 ] Nível médio - você terá 7 chances")
+    print("[ 3 ] Nível difícil - você terá 4 chances")
+    print(" ")
 
-nivel = int(input("Qual nível você deseja escolher"))
+    nivel = int(input("Qual nível você deseja escolher: "))
 
+    if nivel == 1:
+        chance = 10
+    elif nivel == 2:
+        chance = 7
+    elif nivel == 3:
+        chance = 4
+    else:
+        print(" ")
+        print("Opção ínvalida. Vamos começar novamente?")
+        print(" ")
+        time.sleep(1)
+        print("******************************************************")
+        print(" ")
 
 
 palavra_secreta = "BANANA"

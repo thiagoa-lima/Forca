0c287cd63e57ba034b1182eaf96efb5979ce64d6
thiagoandradelima <thiagog_12@hotmail.com>
2023-01-04 08:29:45 -0300

    Melhoria do jogo da forca
    Opção para escolher qual nível a pessoa quer jogar (nível fácil, médio e dificil)
    
    Inclusão de um texto que mostra as letras que ja foram chutadas...
    
    Entre outras

--- jogo.py
+++ jogo.py
@@ -1,5 +1,9 @@
 import time
 
+# ------------------------------------------------------------
+# --------------- MENSAGEM DE ABERTURA DO JOGO ---------------
+# ------------------------------------------------------------
+
 print(" ")
 print("******************************************************")
 print("************* Bem vindo ao jogo da forca *************")
@@ -7,6 +11,7 @@ print("******************************************************")
 print(" ")
 
 time.sleep(1)
+
 nivel = 0
 
 while nivel != 1 and nivel != 2 and nivel != 3:
@@ -20,23 +25,55 @@ while nivel != 1 and nivel != 2 and nivel != 3:
     nivel = int(input("Qual nível você deseja escolher: "))
 
     if nivel == 1:
-        chance = 10
+        chances = 10
     elif nivel == 2:
-        chance = 7
+        chances = 7
     elif nivel == 3:
-        chance = 4
+        chances = 4
     else:
-        print(" ")
-        print("Opção ínvalida. Vamos começar novamente?")
+
+        print("\nOpção ínvalida. Vamos começar novamente?")
         print(" ")
         time.sleep(1)
-        print("******************************************************")
-        print(" ")
+        print("******************************************************\n")
 
 
+# ------------------------------------------------------------
+# ---------------------- Início do jogo ----------------------
+# ------------------------------------------------------------
+
+tentativas = 0
+chances = chances
 palavra_secreta = "BANANA"
-letras_escolhidas = []
 situacao_atual = ["_"] * len(palavra_secreta)
+letras_escolhidas = []
 
-# TRANSFORMA A PALAVRA SECRETA EM UMA LISTA
-palavra_secreta = " ".join(palavra_secreta).split()
+time.sleep(1)
+print(f"Muito bem!! ")
+print(f"A partir de agora você tem {chances} chances para concluir o jogo.")
+print("")
+print("******************************************************")
+print(f"**** VAMOS COMEÇAR? A palavra abaixo tem {len(situacao_atual)} letras ****")
+print("******************************************************")
+print("")
+
+# ------------------------------------------------------------
+# -------------------- Escolha das letras --------------------
+# ------------------------------------------------------------
+
+while tentativas < chances:
+    letra = str(input("Qual letra você gostaria de tentar? ")).upper()
+    letras_escolhidas.append(letra)
+
+    if letra in palavra_secreta:
+        print(f"\nPARABÉNS! A palavra secreta contem a letra {letra.upper()}")
+
+    else:
+        print("QUE PENA! Essa letra não tem na palavra secreta")
+        tentativas += 1
+        print(f"Você já tentou {tentativas} vezes e ainda tem mais {chances - tentativas} chances")
+
+    print(" ".join(situacao_atual).strip())
+    print("As letras que voce ja utilizou foram:")
+    print("  ".join(letras_escolhidas).strip())
+    print("")
